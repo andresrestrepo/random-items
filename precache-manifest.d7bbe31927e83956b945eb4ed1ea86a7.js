b690c85ef3397f1376cfafc8ee685ca377603163
andresrestrepo <andresr.alz@gmail.com>
2020-01-08 17:15:56 -0500

    Updates

---
+++ precache-manifest.d7bbe31927e83956b945eb4ed1ea86a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7fddc7dacf94d28cf698f06ad48883",
+    "url": "/random-items/index.html"
+  },
+  {
+    "revision": "076b5c7b09319afbaf86",
+    "url": "/random-items/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "82c56517a522461dcf24",
+    "url": "/random-items/static/css/main.c2bc2fac.chunk.css"
+  },
+  {
+    "revision": "076b5c7b09319afbaf86",
+    "url": "/random-items/static/js/2.d8662baa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-items/static/js/2.d8662baa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82c56517a522461dcf24",
+    "url": "/random-items/static/js/main.f8ab4a3f.chunk.js"
+  },
+  {
+    "revision": "96d17ac603c3c0b08731",
+    "url": "/random-items/static/js/runtime-main.7ba2fec4.js"
+  }
+]);
